544d7695e2f3dfe191718bc0d9a9a114df92af1d
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-08-26 09:50:07 -0700

    Update site descriptions

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://avanishd-3.github.io/",
+  author: "Avanish Davuluri",
+  profile: "https://avanishd-3.github.io/",
+  desc: "My personal blog",
+  title: "Av Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/avanishd-3/avanishd-3.github.io/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
